46dbbcc656732bce98a148955030035c247737be
parangee <parangee9706@gmail.com>
2020-08-22 12:53:40 +0900

    add github repo link

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,5 +1,13 @@
 module.exports = {
     title: 'DOSBOT DOCS',
     description: '다스봇 문서',
-    dest: 'public'
+    dest: 'public',
+    themeConfig: {
+        nav: [
+            {
+                text: '깃허브',
+                link: 'https://github.com/parangee/dosbot-documentation'
+            }
+        ]
+    }
 }
